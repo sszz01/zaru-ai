373b7db4821e86c9f05a33eda85d763fd9b8f478
sszz01 <resios027@gmail.com>
2025-03-29 00:06:31 -0700

    upd loading animation

--- src/App.tsx
+++ src/App.tsx
@@ -132,12 +132,59 @@ const App: React.FC = () => {
                   className="w-6 h-6 rounded-full"
                 />
                 <div className="ml-2 px-4 py-2 rounded-lg inline-block rounded-bl-none bg-gray-300 text-gray-600">
-                  <div className="newtons-cradle">
-                    <div className="newtons-cradle__dot"></div>
-                    <div className="newtons-cradle__dot"></div>
-                    <div className="newtons-cradle__dot"></div>
-                    <div className="newtons-cradle__dot"></div>
-                  </div>
+                  <svg
+                    className="pl"
+                    width="240"
+                    height="240"
+                    viewBox="0 0 240 240"
+                  >
+                    <circle
+                      className="pl__ring pl__ring--a"
+                      cx="120"
+                      cy="120"
+                      r="105"
+                      fill="none"
+                      stroke="#000"
+                      stroke-width="20"
+                      stroke-dasharray="0 660"
+                      stroke-dashoffset="-330"
+                      stroke-linecap="round"
+                    ></circle>
+                    <circle
+                      className="pl__ring pl__ring--b"
+                      cx="120"
+                      cy="120"
+                      r="35"
+                      fill="none"
+                      stroke="#000"
+                      stroke-width="20"
+                      stroke-dasharray="0 220"
+                      stroke-dashoffset="-110"
+                      stroke-linecap="round"
+                    ></circle>
+                    <circle
+                      className="pl__ring pl__ring--c"
+                      cx="85"
+                      cy="120"
+                      r="70"
+                      fill="none"
+                      stroke="#000"
+                      stroke-width="20"
+                      stroke-dasharray="0 440"
+                      stroke-linecap="round"
+                    ></circle>
+                    <circle
+                      className="pl__ring pl__ring--d"
+                      cx="155"
+                      cy="120"
+                      r="70"
+                      fill="none"
+                      stroke="#000"
+                      stroke-width="20"
+                      stroke-dasharray="0 440"
+                      stroke-linecap="round"
+                    ></circle>
+                  </svg>
                 </div>
               </div>
             )}
