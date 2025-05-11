bfbcf7ce6728b29139bf34c9a1145d00d6f88efb
Semajic <122255922+Semajic@users.noreply.github.com>
2025-05-10 19:22:29 -0700

    experiment w/ Ant Design

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -4,8 +4,9 @@ import { RotateLoader } from "react-spinners";
 import { auth } from "../firebase/firebase";
 import { doc, getDoc } from "firebase/firestore";
 import { db } from "../firebase/firebase";
-import MenuItem from "@mui/material/MenuItem";
-
+import { Menu } from "antd";
+import { AppstoreOutlined, MailOutlined, SettingOutlined } from '@ant-design/icons';
+import type { MenuProps } from 'antd';
 
 
 interface DashProps {
@@ -63,6 +64,82 @@ const Dashboard: React.FC<DashProps> = ({
 
     fetchUserData();
   }, [userPhotoURL]);
+
+  type MenuItem = Required<MenuProps>['items'][number];
+
+  const items: MenuItem[] = [
+    { key: '15', label: 'Profile Dashboard', style: {fontFamily: "Montserrat, sans-serif", fontWeight: 500}, },
+    {
+      key: 'sub1',
+      label: 'Class Periods',
+      style: {fontFamily: "Montserrat, sans-serif", fontWeight: 500},
+      children: [
+        {
+          key: 'g1',
+          label: 'Odd Day',
+          type: 'group',
+          children: [
+            { key: '1', label: 'Period 1' },
+            { key: '2', label: 'Period 3' },
+            { key: '3', label: 'Period 5' },
+            { key: '4', label: 'Period 7' },
+          ],
+        },
+        {
+          key: 'g2',
+          label: 'Even Day',
+          type: 'group',
+          children: [
+            { key: '5', label: 'Period 2' },
+            { key: '6', label: 'Period 4' },
+            { key: '7', label: 'Period 6' },
+            { key: '8', label: 'Period 8' },
+          ],
+        },
+      ],
+    },
+    {
+      key: 'sub2',
+      label: 'Navigation Two',
+      icon: <AppstoreOutlined />,
+      style: {fontWeight: 600},
+      children: [
+        { key: '5', label: 'Option 5' },
+        { key: '6', label: 'Option 6' },
+        {
+          key: 'sub3',
+          label: 'Submenu',
+          children: [
+            { key: '7', label: 'Option 7' },
+            { key: '8', label: 'Option 8' },
+          ],
+        },
+      ],
+    },
+    {
+      type: 'divider',
+    },
+    {
+      key: 'sub4',
+      label: 'Navigation Three',
+      icon: <SettingOutlined />,
+      children: [
+        { key: '9', label: 'Option 9' },
+        { key: '10', label: 'Option 10' },
+        { key: '11', label: 'Option 11' },
+        { key: '12', label: 'Option 12' },
+      ],
+    },
+    {
+      key: 'grp',
+      label: 'Group',
+      type: 'group',
+      children: [
+        { key: '13', label: 'Option 13' },
+        { key: '14', label: 'Option 14' },
+      ],
+    },
+  ];
   
   return (
     <div style={{ 
@@ -71,7 +148,16 @@ const Dashboard: React.FC<DashProps> = ({
         alignItems: "center", 
         justifyContent: "center", 
     }}>
-        <div style={{ 
+
+    <Menu
+      style={{ width: 256, height: "100%", backgroundColor: '#fafcfd', borderRight: "2px solid #d4e3ea", position: "absolute", bottom: 0, left: 0 }}
+      defaultSelectedKeys={['1']}
+      defaultOpenKeys={['sub1']}
+      mode="inline"
+      items={items}
+      onSelect={({ key }) => console.log(key)}
+    />
+        {/* <div style={{ 
             width: '15vw', 
             height: '100%', 
             backgroundColor: '#fafcfd', 
@@ -104,7 +190,7 @@ const Dashboard: React.FC<DashProps> = ({
                 />  
             </div>
 
-        </div>
+        </div> */}
     </div>
   );
 };
